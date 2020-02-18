2b01872394e1a6d37fa466ee7d97a9b99c5b7417
Rocknar74 <58388426+Rocknar74@users.noreply.github.com>
2020-02-18 16:03:22 +0500

    first commit

--- js/Pathfinder.js
+++ js/Pathfinder.js
@@ -1,6 +1,6 @@
 
-const FOCUS = document.querySelectorAll('.focus');
-const FOCUS_ACTIVE = 'focus-active';
+const HOVER_RACE = document.querySelectorAll('.focus');
+const HOVER_RACE_ACTIVE = 'focus-active';
 
 const CLASS = document.querySelectorAll('.name_class');
 const CLASS_ACTIVE = 'name_class-active';
@@ -19,7 +19,7 @@ const CLASS_BLOCK = document.querySelector(".class_block");
 const SELECT_STATS_BLOCK = document.querySelector(".select_stats_block");
 
 const REROLL_BUTTON = document.querySelector(".rerollDice");
-const REROLL_BUTTON_FOCUS = document.querySelector(".rerollDice-focus");
+const REROLL_BUTTON_HOVER = document.querySelector(".rerollDice-focus");
 
 function button_toggle(button) {
     switch(button) { 
@@ -50,7 +50,7 @@ function add_items (obj, newClassObj, thisObj) {
     });
     thisObj.classList.add(newClassObj);
     tolerance_check(thisObj);
-}
+};
 function tolerance_check(thisObj) {
     switch (thisObj.innerHTML) {
         case("Воин"):
@@ -63,44 +63,41 @@ function tolerance_check(thisObj) {
     };
 };
 
-NEXT_BUTTON.addEventListener('click', () => {
-    CLASS_BLOCK.classList.add("class_block-visible");
-    SELECT_STATS_BLOCK.classList.add("select_stats_block-visible");
-    if (SELECT_STATS_BLOCK.classList.contains("select_stats_block-visible")) {
-        random_stats();
-    }
-    NEXT_BUTTON.disabled = true;
-});
-
-FOCUS.forEach(item => {
+HOVER_RACE.forEach(item => {
     item.addEventListener('click', () => {
-        if (clean_items(FOCUS, FOCUS_ACTIVE, item)) {
-            for (let i = 0; i < FOCUS.length; i++) {
-                if (FOCUS[i].classList.contains(FOCUS_ACTIVE)) {
+        if (clean_items(HOVER_RACE, HOVER_RACE_ACTIVE, item)) {
+            for (let i = 0; i < HOVER_RACE.length; i++) {
+                if (HOVER_RACE[i].classList.contains(HOVER_RACE_ACTIVE)) {
                     switch(i) {
                         case (0):
                             character.A.sex = "Ж";
                             character.A.race = "Дварф";
+                            character.bonus_ability_human = false;
                             break;
                         case (1):
                             character.A.sex = "М";
                             character.A.race = "Дварф";
+                            character.bonus_ability_human = false;
                             break;
                         case (2):
                             character.A.sex = "Ж";
                             character.A.race = "Ельф";
+                            character.bonus_ability_human = false;
                             break;
                         case (3):
                             character.A.sex = "М";
                             character.A.race = "Ельф";
+                            character.bonus_ability_human = false;
                             break;
                         case (4):
                             character.A.sex = "Ж";
                             character.A.race = "Человек";
+                            character.bonus_ability_human = true;
                             break;
                         case (5):
                             character.A.sex = "М";
                             character.A.race = "Человек";
+                            character.bonus_ability_human = true;
                             break;
                     }
                 }
@@ -128,13 +125,12 @@ LOVE_WEAPON_WARRIOR.forEach(item => {
         };
     });
 });
-
-CLASS.forEach(classes => {
-    classes.addEventListener('click', () => {
-        const SELECTED_CONTAINER = classes.parentElement.nextElementSibling;
-        if (clean_items(CLASS, CLASS_ACTIVE, classes)) {
-            CONTAINER_CLASS.forEach(item => {
-                item.classList.remove(CONTAINER_CLASS_ACTIVE);
+CLASS.forEach(item => {
+    item.addEventListener('click', () => {
+        const SELECTED_CONTAINER = item.parentElement.nextElementSibling;
+        if (clean_items(CLASS, CLASS_ACTIVE, item)) {
+            CONTAINER_CLASS.forEach(elem => {
+                elem.classList.remove(CONTAINER_CLASS_ACTIVE);
             });
             SELECTED_CONTAINER.classList.toggle('container_class-active');
             for (let i = 0; i < CLASS.length; i++) {
@@ -149,43 +145,85 @@ CLASS.forEach(classes => {
     });
 });
 
-const STONE_NUMBER = document.querySelectorAll(".stoneNumber");
+
+// CHARACTERISTIC BLOCK
+//===========================================================================
+const STONE_NUMBER = document.querySelectorAll(".stone_number");
+const STONE_NUMBER_HOVER = 'stone_number-focus';
+const RADIO_CHARACTERISTICS = document.querySelectorAll('.radio_characteristics');
+const PERSONAL_CONTAINER_NUMBER = document.querySelectorAll(".number");
 const CONTAINER_STONE_NUMBER = document.querySelectorAll(".containerDrag-number");
-const STONE_TABLE = document.querySelector(".container-stoneNumber");
-var currentItem;
-var switchCase;
+const FLAG_TABLE = document.querySelector(".flag_table");
+const CHARCTERISTIC_MODIF = 'characteristic_modif';
+var current_stone;
+var checked_radio;
+var current_number;
+var previous_number;
+console.log(current_number);
+
+
+RADIO_CHARACTERISTICS.forEach(item => {
+    item.addEventListener('change', () => {
+        if (character.bonus_ability_human) {
+            if (checked_radio) {
+                checked_radio.value = +checked_radio.value - 2;
+                checked_radio.nextElementSibling.classList.remove(CHARCTERISTIC_MODIF);
+            };
+
+            item.value = +item.value + 2;
+            item.nextElementSibling.classList.add(CHARCTERISTIC_MODIF);
+
+            checked_radio = item;
+            console.log(3);
+        };
+    });
+});
 
 STONE_NUMBER.forEach(item => {
-    item.addEventListener('dragstart', function() {
-        currentItem = item;
+    item.addEventListener('dragstart', () => {
+        current_stone = item;
     });
-})
+});
 
+PERSONAL_CONTAINER_NUMBER.forEach(item => {
+    item.addEventListener('click', () => {
+        if (character.bonus_ability_human) {
+            if (!item.previousElementSibling.checked) {
+                
+                if (current_number) {
+                    previous_number.innerHTML = `<img src="images/characteristics/numbers/${+previous_number.firstChild.alt - 2}.png" alt="${+previous_number.firstChild.alt - 2}" draggable="false">`;
+                };
+                current_number = item.firstChild;
+
+                item.innerHTML = `<img src="images/characteristics/numbers/${+current_number.alt + 2}.png" alt="${+current_number.alt + 2}" draggable="false">`;
+                previous_number = item;
+            }; 
+        };
+    });
+});
 CONTAINER_STONE_NUMBER.forEach(item => {
-    item.addEventListener("drop", function() {
+    item.addEventListener("drop", () => {
         if (item.innerHTML) {
-            currentItem.parentElement.appendChild(item.firstChild);
-        } 
-        item.appendChild(currentItem);
-        if (STONE_TABLE.firstElementChild === null) {
+            current_stone.parentElement.appendChild(item.firstChild);
+        };
+        item.appendChild(current_stone);
+        if (FLAG_TABLE.firstElementChild === null) {
             button_toggle(true)
-        }
-    })
-})
-CONTAINER_STONE_NUMBER.forEach(item => {
-    item.addEventListener("dragover", function(event) {
+        };
+    });
+    item.addEventListener("dragover", (event) => {
         event.preventDefault();
-    })
-})
+    });
+});
 
-STONE_TABLE.addEventListener("drop", function() {
-    this.appendChild(currentItem); 
-    button_toggle(false)
-})
+FLAG_TABLE.addEventListener("drop", () => {
+    this.appendChild(current_stone); 
+    button_toggle(false);
+});
 
-STONE_TABLE.addEventListener("dragover", function(event) {
+FLAG_TABLE.addEventListener("dragover", (event) => {
     event.preventDefault();
-})
+});
 
 var reroll = true; //Возмоно ли переопределить случайные характеристики
 var stats = []; //Массив со случаынми характеристиками
@@ -196,18 +234,13 @@ function random_stats() { //Получение случайных характе
     let i = 0;
     let currentStat;
 
-    STONE_NUMBER.forEach(item => {
+    PERSONAL_CONTAINER_NUMBER.forEach(item => {
         stats[i] = Math.round(Math.random() * 15 + 3);
         currentStat = stats[i];
 
-        item.innerHTML = `<span class="currentStat">${currentStat}</span><img class="stoneNumber-focus" src="images/characteristics/stoneNumber-focus.png" alt="" draggable="false">`;
+        item.innerHTML = `<img src="images/characteristics/numbers/${currentStat}.png" alt="${currentStat}" draggable="false">`; // изображение с нужным числом
 
-        if (currentStat > 9) {
-            item.innerHTML += `<img src="images/characteristics/numbers/1.png" alt="" draggable="false">`
-            currentStat -= 10;
-        }
-
-        item.innerHTML += `<img src="images/characteristics/numbers/${currentStat}.png" alt="" draggable="false">`;
+        item.previousElementSibling.value = currentStat;
 
         if (stats[i] > highestStat) {
             highestStat = stats[i];
@@ -216,6 +249,7 @@ function random_stats() { //Получение случайных характе
     });
     
     let sumStats = 0;
+
     for (let i = 0; i < stats.length; i++) {
         switch (stats[i]) {
             case (10):
@@ -280,230 +314,32 @@ function random_stats() { //Получение случайных характе
 
     if (!reroll) {
         REROLL_BUTTON.classList.add("rerollDice-notactive");
-        REROLL_BUTTON_FOCUS.classList.add("rerollDice-focus-notactive");
-    }
+        REROLL_BUTTON_HOVER.classList.add("rerollDice-focus-notactive");
+    };
 }
-
+function clear_characteristic_block() {
+    PERSONAL_CONTAINER_NUMBER.forEach(item => {
+        if (item.classList.contains(CHARCTERISTIC_MODIF)) {
+            item.previousElementSibling.checked = false;
+            item.classList.remove(CHARCTERISTIC_MODIF);
+            item.innerHTML = `<img src="images/characteristics/numbers/${+item.firstChild.alt - 2}.png" alt="${+item.firstChild.alt - 2}" draggable="false">`;
+            current_number = undefined;
+            previous_number = undefined;
+        };
+    });
+};
 REROLL_BUTTON.addEventListener("click", () => {
     if (reroll) {
+        clear_characteristic_block();
         random_stats();     
+    };
+});
+
+NEXT_BUTTON.addEventListener('click', () => {
+    CLASS_BLOCK.classList.add("class_block-visible");
+    SELECT_STATS_BLOCK.classList.add("select_stats_block-visible");
+    if (SELECT_STATS_BLOCK.classList.contains("select_stats_block-visible")) {
+        random_stats();
     }
-})
-
-//Объекты рас
-var dwarf = {
-    A: {
-        race: "Дварф",
-    },
-
-    B: {
-        endurance: 2,
-        wisdom: 2,
-        charisma: -2,
-    },
-
-    V: {  
-        speed: "20 фут.",
-        speed_specificity: "Вас не замедляет вес вашей брони и снаряжения.",
-        racial_traits: {
-            strong: "+1 к попаданию по гоблинам и оркам",
-            night_vision: "60 футов. Вы видите в темноте.",
-            hatred: "+2 к испытаниям ядами, магией, а также уникальными способностями чудовищ.",
-            weapon_race: "Можете использовать боевой молот и топор",
-        }, 
-    },  
-};
-var elf = {
-    A: {
-        race: "Эльф",
-    },
-
-    B: {
-        dexterity: 2,
-        endurance: -2,
-        intelligence: 2,
-    },
-
-    V: {
-        speed: "30 фут.",
-        speed_specificity: "При ношении средней или тяжёлой брони ваша скорость падает до 20 футов в раунд.",
-        racial_traits: {
-            twilight_vision: "При низком уровне освещённости вы видите вдвое дальше.",
-            sleep_resist: "Вы невосприимчивы к усыпляющим магическим атакам.",
-            weapon_race: "Можете использовать: короткий и длинный луки, длинные мечи и рапиры",
-        } 
-    },
-    
-    G: {
-        attencion: 2,
-    },     
-};
-var human = {
-    bonus_ability_human: true,
-
-    A: {
-        race: "Человек",
-    },
-
-    V: {
-        speed: "30 фут.",
-        speed_specificity: "При ношении средней или тяжёлой брони ваша скорость падает до 20 футов в раунд.",
-        racial_traits: {
-            bonus_skill: "Каждый уровень, включая первый, вы получаете дополнительный пункт навыка.", 
-        },
-    },
-
-    E: {
-        skill: 1,
-    },
-};
-
-//Объекты классов
-var warrior = {
-    A: {
-        class: "Воин",
-    },
-
-    D: {
-        shields: true,
-        light_armor: true,
-        medium_armor: true,
-        heavy_armor: true,
-        simple_weapon: true,
-        martial_weapon: true,
-    },
-
-    E: {
-        hp: 10,
-        fortitude: 2,
-        reflex: 0,
-        will: 0,
-        attack_bonus: 1,
-        skill: 2,
-    },
-
-    I: {
-        feats: {
-            warrior_weapon: "",
-        },
-    }
-};
-var wizard = {
-
-};
-var rogue = {
-
-};
-var priest = {
-
-};
-
-
-
-//Итоговый объект персонажа
-var character = {
-    bonus_ability_human: false,
-    A: {
-        name: "", //имя
-        race: "", //раса
-        sex: "", //пол персонажа
-        class: "", //класс
-        ideology: "", //мировоззрение
-        level: 1, //уровень
-    },
-    
-    B: {
-        strength: 0, //сила
-        dexterity: 0, //ловкость
-        endurance: 0, //выносливость
-        intelligence: 0, //интеллект
-        wisdom: 0, //мудрость
-        charisma: 0, //харизма
-        strength_mod: 0,
-        dexterity_mod: 0,
-        endurance_mod: 0,
-        intelligence_mod: 0,
-        wisdom_mod: 0,
-        charisma_mod: 0,
-    },
-
-    V: {
-        speed: "", //скорость
-        speed_specificity: "", //особенность скорости
-        racial_traits: {}, //рассовые особенности
-    },
-
-    G: {
-        acrobatics: 0, //акробатика
-        bluff: 0, //блеф
-        ride: 0, //верховая езда
-        attencion: 0, //внимательность
-        diplomacy: 0, //дипломатия
-        knowleadge: { //знания
-            geography: 0,  //география
-            history: 0, //история
-            local: 0, //краеведение
-            dungeoneering: 0,  //подземелья
-            nature: 0, //природа
-            religion: 0, //религия
-            arcana: 0, //магия
-        },
-        spellcraft: 0, //колдовство
-        climb: 0, //лазание
-        heal: 0, //лечение
-        mechanics: 0, //механника
-        swim: 0, //плавание
-        insight: 0, //проницательность
-        stealth: 0, //скрытность
-    },
-    
-    D: {
-        shields: false, //щиты
-        light_armor: false, //лёгкая броня
-        medium_armor: false, //средняя броня
-        heavy_armor: false, //тяжёлая броня
-        simple_weapon: false, //простое оружие
-        martial_weapon: false, //военное оружие
-        special_weapon: [], //разное оружие
-    },
-
-    E: {
-        hp: 0, //здоровье
-        fortitude: 0, //стойкость
-        reflex: 0, //рефлексы
-        will: 0, //воля
-        attack_bonus: 0, //мод. атаки
-        skill: 0, //пункты навыков
-    },
-
-    J: {
-        initiative: 0,
-        melee_attack: 0,
-        ranged_attack: 0,
-        weapon_1: "",
-        weapon_2: "",
-    },
-
-    Z: {
-        armor_class: 10, // КБ(класс брони)
-    },
-
-    I: {
-        feats: {},
-    },
-
-    L: {
-        spells: [],
-    },
-};
-
-// for (let key in dwarf) {
-//     if (character[key] == undefined) {
-//          character[key] = dwarf[key];
-//     } else {
-//     character[key] += dwarf[key];
-//     }
-// }
-
-// console.log(dwarf);
-// console.log(character);
+    button_toggle(false);
+});
